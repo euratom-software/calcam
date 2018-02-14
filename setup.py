0c2156b7628a663f852d5e1d5fdd4f48e0076eb1
Scott Silburn <s.silburn@outlook.com>
2018-02-14 23:37:48 +0000

    Switch to using setuptools instead of distutils to enable auto script generation.

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ Calcam Setup script.
 Pretty trivial; Calcam is a pure python module with a few data files and a script to start it.
 '''
 
-from distutils.core import setup
+from setuptools import setup,find_packages
 
 setup(
 	  name='Calcam',
 	  version='1.9.0',
 	  url='https://github.com/euratom-software/calcam',
 	  license='European Union Public License 1.1',
-	  packages=['calcam'],
+	  packages=find_packages(),
 	  package_data={'calcam':['ui/*.ui','ui/*.png','usercode_examples/*.py_']},
-	  scripts=['bin/calcam']
+      entry_points={ 'gui_scripts': [ 'calcam = calcam.gui:start_gui'] },
+      zip_safe=False
 	  )
