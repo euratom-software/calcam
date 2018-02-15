86c9d4b395d42be1244eb4351459393ca579dede
Scottt Silburn <s.silburn@ukaea.uk>
2018-02-15 09:00:21 +0000

    Add a printed message in setup for Windows users to tell them where the launcher .exe is.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 '''
 Calcam Setup script.
 
-Pretty trivial; Calcam is a pure python module with a few data files and a script to start it.
+Fairly usual setuptools setup and then tell Windows users where the launcher exe was created.
 '''
 
 from setuptools import setup,find_packages
+import sys
 
 setup(
 	  name='Calcam',
@@ -13,6 +14,14 @@ setup(
 	  license='European Union Public License 1.1',
 	  packages=find_packages(),
 	  package_data={'calcam':['ui/*.ui','ui/*.png','usercode_examples/*.py_']},
-      entry_points={ 'gui_scripts': [ 'calcam = calcam.gui:start_gui'] },
-      zip_safe=False
+          entry_points={ 'gui_scripts': [ 'calcam = calcam.gui:start_gui'] },
+          zip_safe=False
 	  )
+	 
+
+if len(sys.argv) > 1:
+    if sys.platform == 'win32' and (sys.argv[1] == 'install' or sys.argv[1] == 'develop'):
+        import sysconfig
+        import os
+        script_dir = sysconfig.get_path('scripts')
+        print('\n****\nPath to Calcam GUI launcher:\n"{:s}"\n****\n'.format(os.path.join(script_dir,'calcam.exe')))
