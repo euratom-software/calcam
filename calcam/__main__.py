a4787b328bf70ce0d4281b6928a3fb05c0583baa
Scott Silburn <s.silburn@outlook.com>
2025-10-13 20:46:53 +0100

    Hopefully fix -m launching implementation on Windows by moving gui/launch_script.py to __main__.py

--- calcam/__main__.py
+++ calcam/__main__.py
@@ -1,1 +1,63 @@
-gui/launch_script.py
+'''
+* Copyright 2015-2018 European Atomic Energy Community (EURATOM)
+*
+* Licensed under the EUPL, Version 1.1 or - as soon they
+  will be approved by the European Commission - subsequent
+  versions of the EUPL (the "Licence");
+* You may not use this work except in compliance with the
+  Licence.
+* You may obtain a copy of the Licence at:
+*
+* https://joinup.ec.europa.eu/software/page/eupl
+*
+* Unless required by applicable law or agreed to in
+  writing, software distributed under the Licence is
+  distributed on an "AS IS" basis,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+  express or implied.
+* See the Licence for the specific language governing
+  permissions and limitations under the Licence.
+'''
+
+import sys
+try:
+    from . import gui
+except:
+    from calcam import no_gui_reason
+    gui = False
+
+if __name__ == '__main__':
+
+    if not gui:
+        try:
+            from tkinter import messagebox
+            messagebox.showerror(title='Cannot start Calcam GUI',message='Cannot open calcam window: {:s}'.format(no_gui_reason))
+        except Exception:
+            print('Cannot open calcam window: {:s}'.format(no_gui_reason))
+        exit()
+        
+    try:
+        arg = sys.argv[1]
+    except:
+        arg = '--launcher'
+
+    if arg == '--launcher':
+        gui.open_window(gui.Launcher)
+    elif arg == '--fitting_calib':
+        gui.open_window(gui.FittingCalib)
+    elif arg == '--alignment_calib':
+        gui.open_window(gui.AlignmentCalib)
+    elif arg == '--virtual_calib':
+        gui.open_window(gui.VirtualCalib)
+    elif arg == '--viewer':
+        gui.open_window(gui.Viewer)
+    elif arg == '--image_analyser':
+        gui.open_window(gui.ImageAnalyser)
+    elif arg == '--settings':
+        gui.open_window(gui.Settings)
+    elif arg == '--cad_edit':
+        try:
+            filepath = sys.argv[2]
+        except:
+            filepath = None
+        gui.open_window(gui.CADEdit,filepath)
